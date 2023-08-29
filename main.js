f9984341ca8e042c5464b00575f9721af53abbbf
apitchaya2545 <137855729+apitchaya2545@users.noreply.github.com>
2023-08-29 23:23:14 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,55 @@
+var number1 = document.getElementById('number1');
+var number2 = document.getElementById('number2');
+var mark = document.getElementById('mark');
+var answer = document.getElementById('answer');
+var result = document.getElementById('result');
+var scores = document.getElementById('score');
+
+var num1, num2, sum, score = 0, point = 0;
+scores.innerHTML = score + "/" + point;
+function random() {
+    num1 = Math.floor(Math.random() * 11);
+    num2 = Math.floor(Math.random() * 11);
+    number1.value = num1;
+    number2.value = num2;
+    answer.value = '';
+}
+
+function start() {
+    point = 0;
+    score = 0;
+    scores.innerHTML = score + "/" + point;
+    random();
+}
+
+function selectmark(value) {
+    mark.innerHTML = value;
+}
+
+function markcheck() {
+    if (mark.innerHTML == "+") {
+        sum = num1 + num2;
+    } else if (mark.innerHTML == "-") {
+        sum = num1 - num2;
+    } else if (mark.innerHTML == "×") {
+        sum = num1 * num2;
+    } else if (mark.innerHTML == "÷") {
+        sum = num1 / num2;
+    }
+}
+
+function response() {
+    markcheck();
+    console.log('sum : ' + sum);
+    if (sum == answer.value) {
+        result.innerHTML = 'ถูกต้อง!!!';
+        score += 1;
+    } else {
+        result.innerHTML = 'ไม่ถูกต้อง!!!';
+    }
+    point += 1;
+    scores.innerHTML = score + "/" + point;
+    random();
+}
+
+random();
